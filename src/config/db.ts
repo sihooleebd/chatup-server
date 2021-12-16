2f0130fa801e34187886f2d24167aff6324cce9e
Benjamin <sihooleebd@gmail.com>
2021-12-17 00:06:06 +0900

    new db loc

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,8 +1,20 @@
+// const dbConfig = {
+//   username: "benjamin",
+//   password: "mysqlpw0807",
+//   database: "my_blog_service",
+//   host: "127.0.0.1",
+// };
+
+
+
+// export default dbConfig;
+
+
 const dbConfig = {
   username: "benjamin",
-  password: "mysqlpw0807",
-  database: "my_blog_service",
-  host: "127.0.0.1",
+  password: "ubUntUSerVer1357!",
+  database: "chatup",
+  host: "192.168.50.102",
 };
 
 
