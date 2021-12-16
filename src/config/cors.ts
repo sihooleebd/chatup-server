84707e8c3b6826d0226bf8d662e91f28ce119ab5
Benjamin <sihooleebd@gmail.com>
2021-12-17 00:15:27 +0900

    cors 추가

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,4 +1,4 @@
-const whiteList = ['http://localhost:1234', 'http://192.168.50.171:1234', 'http://192.168.2.171:1234', 'http://192.168.200.130:1234'];
+const whiteList = ['http://localhost:1234', 'http://192.168.50.171:1234', 'http://192.168.2.171:1234', 'http://192.168.200.130:1234', 'http://chatup.mcjunon.com'];
 
 const corsConfig = {
   origin: whiteList,
