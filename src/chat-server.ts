93337962268149c6e1b63a97d3f7957638b20ce1
Benjamin <sihooleebd@gmail.com>
2021-12-26 20:54:23 +0900

    출력 추가

--- src/chat-server.ts
+++ src/chat-server.ts
@@ -37,6 +37,7 @@ export default class ChatServer {
 
 
       this.writeMessageToDb(roomId, connectedUserId, message).then( (result) => {
+        console.log('socket --> ', socket);
         socket.to(roomId.toString()).emit('chatMessage', {
         // socket.emit('chatMessage', {
           message,
