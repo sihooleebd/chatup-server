e6fa9a66fee6ddefeddaaace942841576cc157a2
Benjamin <sihooleebd@gmail.com>
2022-01-30 18:35:24 +0900

    env 설정과 delete 기능 추가

---
+++ src/config/env.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import dotenv from 'dotenv';
+const envFilePath = `${path.resolve()}/.env/${process.env.NODE_ENV}.env`;
+
+dotenv.config({ path: envFilePath });
+
+export function getEnv(name:string, type: 'string'|'number'|'boolean') {
+  const val = process.env[name];
+  if(val===undefined) return null;
+  
+  if(type==='string') return val;
+  if(type==='number') return parseInt(val);
+  return (val==='true');
+}
